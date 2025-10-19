cc76a88c48f266f24be141ac3d1ecdf646aaaa89
Scott Zhang <fzhan113@asu.edu>
2025-10-18 20:11:52 -0700

    update essay result

--- cbm/main.py
+++ cbm/main.py
@@ -56,6 +56,7 @@ BASE_RUN = RunConfig(
 # Which datasets and models to run
 DATASETS = ["essay"]  # choose from: "cebab", "imdb", "essay"
 MODELS = ["bert-base-uncased", "roberta-base", "gpt2", "lstm"]
+MODELS = ["bert-base-uncased", "roberta-base"]
 
 # Output CSV path
 OUTPUT_CSV = os.path.join(MAIN_DIR, "result_essay.csv")
